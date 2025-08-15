7abeb0f79328de51361db278032e2ccc1d689c57
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-08-15 17:01:44 +0600

    Создал двух наследников

--- hw_2.py
+++ hw_2.py
@@ -4,3 +4,16 @@ class Person:
         self.birth_date = birth_date
         self.occupation = occupation
         self.higher_education = higher_education
+
+class Friend(Person):
+    def introduce(self):
+        print(f"Привет, меня зовут {self.name}, я друг Игоря, "
+              f"я родился {self.birth_date}, работаю {self.occupation.lower()}.")
+
+
+
+class Classmate(Person):
+    def introduce(self):
+        print(f"Привет, меня зовут {self.name}, я одноклассник Игоря, "
+              f"я родился {self.birth_date}, работаю {self.occupation.lower()}.")
+

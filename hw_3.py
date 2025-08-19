3739bf99b641f2dd169cd6eaba6f3438c3d88ad9
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-08-19 16:51:53 +0600

    Сделал occupation и higher_education приватными

---
+++ hw_3.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, birth_date, occupation, higher_education):
+        self.name = name
+        self.birth_date = birth_date
+        self.__occupation = occupation
+        self.__higher_education = higher_education
+
+    @property
+    def occupation(self):
+        return self.__occupation
+
+    @property
+    def higher_education(self):
+        return self.__higher_education
